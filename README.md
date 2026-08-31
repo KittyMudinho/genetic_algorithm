575c2ebd7ef950912bcdcf954cff2f5151e013cf
KittyMudinho <cientistadacompgustavosampaio@gmail.com>
2026-08-31 17:49:20 -0300

    Improving code and adding plot of the errors

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 <html>
 <h1>GENETIC ALGORITHM</h1>
 <h2>This's made with python using numpy.</h2>
-<h3>To do the crossover, I've made something inspired by OX (order crossover).</h3>
+<h3>To do the crossover, I've made one-point cut crossover, because it's just for words.</h3>
 </html>
--- ag.py
+++ ag.py
@@ -1,64 +1,53 @@
 
 import numpy as np
 import random
+import matplotlib.pyplot as plt
 
-
-
-QUOTE='GUSTAVO'
+errors=[]
+QUOTE='CASA'
 TAM_CROMO=len(QUOTE)
-TAM_POP=TAM_CROMO*4
+TAM_POP=TAM_CROMO*32
 
 
 pop_init=np.zeros((TAM_POP,TAM_CROMO))
 
 def init_pop():
-    global pop_init
-    global TAM_POP
-    global TAM_CROMO
+    global pop_init, TAM_POP, TAM_CROMO
     for i in range(TAM_POP):
         pop_init[i]=np.random.randint(low=65,high=90,size=TAM_CROMO)
 
 def cruza(dad1,dad2):
-    global pop_init
-    global TAM_CROMO
+    global pop_init, TAM_CROMO
     pos_dad1=dad1
     pos_dad2=dad2
     dad1=pop_init[dad1]
     dad2=pop_init[dad2]
     son1=[0]*TAM_CROMO
     son2=[0]*TAM_CROMO
-    son1[1]=dad1[1]
-    son1[2]=dad1[2]
-    son2[1]=dad2[1]
-    son2[2]=dad2[2]
+    random_pos=random.sample(range(1,TAM_CROMO),1)[0]
+    for i in range(random_pos,TAM_CROMO):
+        son1[i]=dad1[i]
+        son2[i]=dad2[i]
     pos=0
     for i in dad2:
-        if i not in son1:
+        if pos not in range(random_pos,TAM_CROMO):
             son1[pos]=i
-            pos=3
+            pos+=1
     pos=0
     for i in dad1:
-        if i not in son2:
+        if pos not in range(random_pos,TAM_CROMO):
             son2[pos]=i
-            pos=3
+            pos+=1
     pop_init[pos_dad1]=son1
     pop_init[pos_dad2]=son2
 
-def no_rep(dad1,dad2):
-    for i in dad1:
-        if i in dad2:return False
-    return True
-
 def mk(val_quote):
-    global pop_init
+    global pop_init, TAM_CROMO
     dad1=0
     dad2=0
-    while True:
-        pos=random.sample(range(len(val_quote)),4)
-        dad1 = pos[0] if val_quote[pos[0]] <= val_quote[pos[1]] else pos[1]
-        dad2 = pos[2] if val_quote[pos[2]] <= val_quote[pos[3]] else pos[3]
-        
-        if dad1!=dad2 and no_rep(pop_init[dad1],pop_init[dad2])==True:break
+    pos=random.sample(range(TAM_CROMO),4)
+    dad1 = pos[0] if val_quote[pos[0]] <= val_quote[pos[1]] else pos[1]
+    dad2 = pos[2] if val_quote[pos[2]] <= val_quote[pos[3]] else pos[3]
     return dad1,dad2
 
 def fitness():
@@ -81,12 +70,13 @@ def printar_pop():
     print(pop_init)
 
 def print_best(val_quote):
-    global pop_init
+    global pop_init,errors
     pos_min = np.argmin(val_quote)
     print('Best: ', end='')
     for i in pop_init[pos_min]:
         print(chr(int(i)), end='')
     print(f' | Erro: {val_quote[pos_min]}')
+    errors.append(val_quote[pos_min])
 
 if __name__=='__main__':
     init_pop()
@@ -98,5 +88,12 @@ if __name__=='__main__':
         if min(val_quote)==0:break
         dad1,dad2=mk(val_quote)
         cruza(dad1,dad2)
-        mutacao(0.2)
+        mutacao(0.1)
         ger+=1
+    plt.plot(errors, marker='o', linestyle='-', color='blue')
+
+    plt.xlabel('Geração')
+    plt.ylabel('Erro')
+    plt.title('Erro por geração')
+    plt.grid(True, linestyle='--', alpha=0.6)
+    plt.show()
