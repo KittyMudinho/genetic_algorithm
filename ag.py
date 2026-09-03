736664af43312d5520411a4aebf14560369d3a05
KittyMudinho <cientistadacompgustavosampaio@gmail.com>
2026-09-03 17:43:25 -0300

    Putting elitism

--- ag.py
+++ ag.py
@@ -1,13 +1,12 @@
-
 import numpy as np
 import random
 import matplotlib.pyplot as plt
 
 errors=[]
-QUOTE='CASA'
+QUOTE='GUSTAVO'
 TAM_CROMO=len(QUOTE)
-TAM_POP=TAM_CROMO*32
-
+TAM_POP=TAM_CROMO*16
+taxa_melhor=int(TAM_POP*0.10)
 
 pop_init=np.zeros((TAM_POP,TAM_CROMO))
 
@@ -41,11 +40,16 @@ def cruza(dad1,dad2):
     pop_init[pos_dad1]=son1
     pop_init[pos_dad2]=son2
 
-def mk(val_quote):
+def mk(val_quote,latest_dads):
     global pop_init, TAM_CROMO
     dad1=0
     dad2=0
-    pos=random.sample(range(TAM_CROMO),4)
+    aval_dads=[]
+    for i in range(TAM_POP):
+        if i not in latest_dads:aval_dads.append(i)
+    if len(aval_dads)==2:
+        return aval_dads[0],aval_dads[1]
+    pos=random.sample(aval_dads,4)
     dad1 = pos[0] if val_quote[pos[0]] <= val_quote[pos[1]] else pos[1]
     dad2 = pos[2] if val_quote[pos[2]] <= val_quote[pos[3]] else pos[3]
     return dad1,dad2
@@ -57,6 +61,21 @@ def fitness():
         for j in range(TAM_CROMO):
             val_quote[i] += (ord(QUOTE[j]) - pop_init[i][j])**2
     return val_quote
+
+def best_elements(val_quote):
+    global pop_init,taxa_melhor
+    best=[]
+    for i in range(len(pop_init)):
+        if i in np.argsort(val_quote)[:taxa_melhor]:best.append(pop_init[i])
+    return best
+
+def elitismo(val_quote,best):
+    global pop_init,taxa_melhor
+    piores=np.argsort(val_quote)[len(val_quote)-taxa_melhor:len(val_quote)]
+    pos=0
+    for i in range(len(pop_init)):
+        if i in piores:
+            pop_init[i]=best[pos]
 
 def mutacao(taxa=0.1):
     global pop_init, TAM_POP, TAM_CROMO
@@ -81,13 +100,21 @@ def print_best(val_quote):
 if __name__=='__main__':
     init_pop()
     ger=0
+    latest_dads=[]
     while True:
+        latest_dads=[]
         print(f'Geração: {ger}')
         val_quote=fitness()
         print_best(val_quote)
+        best=best_elements(val_quote)
         if min(val_quote)==0:break
-        dad1,dad2=mk(val_quote)
-        cruza(dad1,dad2)
+        while len(latest_dads)!=TAM_POP:
+            dad1,dad2=mk(val_quote,latest_dads)
+            latest_dads.append(dad1)
+            latest_dads.append(dad2)
+            cruza(dad1,dad2)
+        val_quote=fitness()
+        elitismo(val_quote,best)
         mutacao(0.1)
         ger+=1
     plt.plot(errors, marker='o', linestyle='-', color='blue')
